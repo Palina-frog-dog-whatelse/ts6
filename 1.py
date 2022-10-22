03917d09c98c9c658ec66b48eb30a721f05e5b0c
Palina <114215521+Palina-frog-dog-whatelse@users.noreply.github.com>
2022-10-22 17:03:13 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,15 +1,1 @@
-n = int(input())
-array = [float(i) for i in input().split()]
-imin = 0
-imax = 0
-def make_zero():
-    for i in range(1, n):
-        if (array[i] >= array[imin]):
-            imin = i
-    for i in range (imin + 1, n):
-        if (array[i] >= array[imax]):
-            imax = i
-    for i in range (imax , n):
-        array[i] = 0
-    print(array)
-meke_zero()
+
