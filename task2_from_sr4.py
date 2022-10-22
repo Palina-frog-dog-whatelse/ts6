fcaa942c9c1a38b8c7a0ae1b389ec30f4ba66c40
Palina <114215521+Palina-frog-dog-whatelse@users.noreply.github.com>
2022-10-22 17:01:59 +0300

    Update task2_from_sr4.py

--- task2_from_sr4.py
+++ task2_from_sr4.py
@@ -1,11 +1,11 @@
-def create_massiv(m):
+def create_massiv(m): #функция, которая запрашивает два массива
     size = (int(input("Введите размер массива " + m + " ")))
     massiv = []
     for i in range(0,size):
-        massiv.append((input("Введите один элемен массива " + m + " " )))
+        massiv.append((input("Введите один элемент массива " + m + " " )))
     return massiv
 massiv_A = create_massiv("A")
 massiv_B = create_massiv("B")
-ans = list(set(massiv_A).intersection(massiv_B))
+ans = list(set(massiv_A).intersection(massiv_B)) #intersection находит общие у массивов элементы
 print(ans)
         
